db79fc729c06eea1172b4f5f86cb2c823c741942
Judah Perez <tythin4@gmail.com>
2018-01-27 15:24:00 -0800

    ping sound

---
+++ Assets/Scripts/PlayerAudio.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAudio : MonoBehaviour
+{
+    [SerializeField]
+    private List<AudioClip> _clips = new List<AudioClip>();
+    [SerializeField]
+    private AudioSource _audio;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnNoise(int soundId)
+    {
+        _audio.clip = _clips[Mathf.Min(soundId, _clips.Count)];
+        _audio.Play();
+    }
+
+}
