f3dd16851cc656608b8497ea90858b6087858812
Judah Perez <tythin4@gmail.com>
2018-01-26 22:52:53 -0800

    prototyping unity assets and player movement controls

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class CameraFollow : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform _target;
+
+        [SerializeField]
+        private float _height = 10;
+        [SerializeField]
+        private float _moveSpeed = 2;
+
+        // Use this for initialization
+        void Start () {
+		
+        }
+	
+        // Update is called once per frame
+        void Update ()
+        {
+            var targetPos = _target.position;
+            targetPos.y += _height;
+            transform.position = Vector3.Lerp(transform.position, targetPos, Time.deltaTime * _moveSpeed);
+        }
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,30 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+namespace Assets.Scripts
+{
+    [RequireComponent(typeof(CharacterController))]
+    public class PlayerMovement : MonoBehaviour
+    {
+        [SerializeField]
+        private CharacterController _characterController;
+        [SerializeField]
+        private float _movementSpeed = 5f;
 
-	// Use this for initialization
-	void Start () {
+        // Use this for initialization
+        void Start () {
 		
-	}
+        }
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        // Update is called once per frame
+        void Update () {
+            var movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            if (movement.magnitude > 1)
+            {
+                movement.Normalize();
+            }
+
+            movement *= _movementSpeed;
+            _characterController.SimpleMove(movement);
+        }
+    }
 }
