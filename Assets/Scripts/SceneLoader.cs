fa85f61d5d370142da72e2676333bc9e65db0d5d
Brent Arata <brent.arata@gmail.com>
2018-01-27 12:34:21 -0800

    Adding the main menu to the game.

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnClick(string type)
+    {
+        StartCoroutine(LoadGameScene());
+        
+    }
+
+    private IEnumerator LoadGameScene()
+    {
+        yield return new WaitForSeconds(3);
+
+        //TODO: Abstract this if we need to load a different scene.
+        AsyncOperation async = SceneManager.LoadSceneAsync("Movement");
+
+        while (!async.isDone)
+        {
+            yield return null;
+        }
+    }
+}
