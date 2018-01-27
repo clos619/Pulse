325adf4c7ee7f1ac5d5b935f4c70a0fa26d9a709
Alec Asperslag <aaspersl@ucsc.edu>
2018-01-27 10:34:27 -0800

    Added a basic player ping
    Comes with an event anything can subscribe to like the movement script

---
+++ Assets/Scripts/TestScripts/TestPingReact.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPingReact : MonoBehaviour {
+
+    public ParticleSystem thecoolpingableparticle;
+
+	// Use this for initialization
+	void Start () {
+        thecoolpingableparticle = GetComponent<ParticleSystem>();
+	}
+	
+	public void UsePing(Transform playerTransform)
+    {
+        if (thecoolpingableparticle.isPlaying)
+            thecoolpingableparticle.Stop();
+        thecoolpingableparticle.transform.position = playerTransform.position;
+        thecoolpingableparticle.Play();
+    }
+}
