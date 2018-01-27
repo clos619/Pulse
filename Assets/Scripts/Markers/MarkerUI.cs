ddd0da9ba5cc36ca9d6f67627763a4e195244d9c
Judah Perez <tythin4@gmail.com>
2018-01-27 10:21:22 -0800

    Marker Generation

---
+++ Assets/Scripts/Markers/MarkerUI.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Markers
+{
+    public class MarkerUI : MonoBehaviour
+    {
+        public Image Image;
+
+        public Marker Marker;
+
+        private RectTransform _rectTransform; 
+
+        // Use this for initialization
+        void Start ()
+        {
+            _rectTransform = GetComponent<RectTransform>();
+        }
+	
+        // Update is called once per frame
+        void Update ()
+        {
+            _rectTransform.anchorMax = Marker.OnScreenPos;
+            _rectTransform.anchorMin = Marker.OnScreenPos;
+        }
+    }
+}
---
+++ Assets/Scripts/Markers/MarkersUI.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Markers
+{
+    public class MarkersUI : MonoBehaviour
+    {
+        public static MarkersUI Instance;
+
+        private MarkerUI _markerPrefab;
+        [SerializeField]
+        private List<MarkerUI> _markers= new List<MarkerUI>();
+
+        // Use this for initialization
+        void Awake ()
+        {
+            Instance = this;
+        }
+	
+        // Update is called once per frame
+        void Update () {
+		
+        }
+
+
+        public void OnMarkerUpdate(Marker marker)
+        {
+            MarkerUI markerUI = null;
+            for (int cnt = 0; cnt < _markers.Count; cnt++)
+            {
+                if (_markers[cnt].Marker == marker)
+                {
+                    markerUI = _markers[cnt];
+                }
+            }
+
+            if (markerUI == null)
+            {
+                if(marker.Seen == false)
+                    return;
+
+                markerUI = Instantiate(_markerPrefab, transform);
+                markerUI.Marker = marker;
+                _markers.Add(markerUI);
+
+            }
+
+        }
+    }
+}
