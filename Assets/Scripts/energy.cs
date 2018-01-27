df2d5ccc0525100842e3593155c5d03d68fbe60f
clos619 <clos619@users.noreply.github.com>
2018-01-27 00:08:04 -0800

    ui script

---
+++ Assets/Scripts/energy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Assets.Scripts;
+
+public class energy : MonoBehaviour {
+
+    public int startingEnergy = 1500;
+    public int currentEnergy;
+    public Slider energySlider;
+    bool isMoving;
+    public PlayerStats PlayerStats;
+	// Use this for initialization
+	void Start () {
+        currentEnergy = startingEnergy;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        loseEnergy();
+
+	}
+    void loseEnergy()
+    {
+        if(isMoving == true)
+        {
+            currentEnergy = currentEnergy - 5;
+        }
+        
+        
+        
+    }
+
+}
