e36574e106f8bb1d03b5e58c93e7c1f7fbaa7dc8
Brent Arata <brent.arata@gmail.com>
2018-01-27 16:49:58 -0800

    Adding the pulsing and fade out effects for the main menu scene.

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -4,6 +4,10 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour {
 
+    public GameObject LoadingText;
+    public GameObject Logo;
+    public GameObject LoadingParticles;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -23,9 +27,13 @@ public class SceneLoader : MonoBehaviour {
     private IEnumerator LoadGameScene()
     {
         yield return new WaitForSeconds(3);
+        Logo.SetActive(false);
+        Debug.Log("WTF", LoadingText);
+        //LoadingText.SetActive(true);
+        LoadingParticles.SetActive(true);
 
         //TODO: Abstract this if we need to load a different scene.
-        AsyncOperation async = SceneManager.LoadSceneAsync("Movement");
+        AsyncOperation async = SceneManager.LoadSceneAsync("Game");
 
         while (!async.isDone)
         {
