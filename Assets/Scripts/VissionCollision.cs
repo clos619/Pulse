36fd0567de5b86f383b56ffee8caca4c5b678fe4
BlueRider0 <adanielson@gmail.com>
2018-01-27 23:16:11 -0800

    Frickin Laser Beams
    Monsters now shoot a laser at the player when spotted.  It's a little wonky, I'll fix it tomorrow.

--- Assets/Scripts/VissionCollision.cs
+++ Assets/Scripts/VissionCollision.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class VissionCollision : MonoBehaviour {
 
-    private GameOverManager gameOver;
+    private bool gameOver = false;
+    private float countdown = 0f;
 
 	// Use this for initialization
 	void Start () {
@@ -13,15 +14,35 @@ public class VissionCollision : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (gameOver)
+        {
+            if (countdown < 1f)
+            {
+                countdown += Time.deltaTime;
+            }
+            else
+            {
+                GameOverManager.Instance.GameOver();
+            }
+        }
 	}
 
     void OnTriggerEnter(Collider col)
     {
         if (col.gameObject.tag == "Player")
         {
+            GetComponent<MeshRenderer>().enabled = false;
+            Vector3 destinationDirection = (col.gameObject.transform.position - this.gameObject.transform.position).normalized;
+            this.gameObject.transform.rotation = Quaternion.LookRotation(destinationDirection);
+
+            this.gameObject.transform.Find("GeroBeam").gameObject.active = true;
+            Monster monster = this.transform.parent.gameObject.GetComponent<Monster>();
+
+            monster.Stop();
+
+            gameOver = true;
             // Call death animation/game over stuff
-            GameOverManager.Instance.GameOver();
+            //GameOverManager.Instance.GameOver();
         }
     }
 
