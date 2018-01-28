ca5f3bc602117605a4584c5e2d7e3838ecca058b
clos619 <clos619@users.noreply.github.com>
2018-01-28 15:40:25 -0800

    changed enemy values

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -6,13 +6,13 @@ using UnityEngine;
 
 public class Monster : MonoBehaviour {
     // What it says on the tin.
-    public float movementSpeed = 4f;
-    public float rotationSpeed = 45f; //degrees per second
+    public float movementSpeed = 500.0f;
+    public float rotationSpeed = 105f; //degrees per second
                                       // at 45 does a 360 in 8 seconds.
 
     // Monsters should probably be faster if they're actually chasing something.
-    public float trackMovementMultiplier = 1.5f;
-    public float trackRotationMultiplier = 1.5f;
+    public float trackMovementMultiplier = 600.0f;
+    public float trackRotationMultiplier = 150.5f;
     
     // Movement and rotation speed when tracking a ping.
     // Assuming monsters should move faster when tracking.
