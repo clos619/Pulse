4369c61a65f46643b8754b0e818a4de86d6de315
Alec Asperslag <aaspersl@ucsc.edu>
2018-01-27 22:09:30 -0800

    Okay

---
+++ Assets/Scripts/energyBoost.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Scripts;
+using UnityEngine.UI;
+
+public class energyBoost : MonoBehaviour {
+
+    public float boostEnergy = 100;
+    public GameObject erg;
+    public PlayerStats playerstats;
+    public energyUI life;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+    void onTriggerEvent(Collider erg)
+    {
+        if(erg.gameObject.CompareTag("Pick Up"))
+        {
+            erg.gameObject.SetActive(true);
+
+            life.energySlider.size = playerstats.Energy + boostEnergy;
+
+        }
+        if (erg.GetComponent<Collider>())
+        {
+            erg.gameObject.SetActive(false);
+        }
+    }
+}
