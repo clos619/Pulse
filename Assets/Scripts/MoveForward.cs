4cd5b8df82fbaa7f93bfd803b316ea5668eb1ca7
Judah Perez <tythin4@gmail.com>
2018-01-27 22:07:58 -0800

    Flying Ship Away

---
+++ Assets/Scripts/MoveForward.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForward : MonoBehaviour
+{
+
+    [SerializeField] private float _speed = 5;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    transform.position += transform.forward * _speed * Time.deltaTime;
+	}
+}
